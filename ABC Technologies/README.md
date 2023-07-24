0643729ed4d4bc721582d753d8f936968c0c9b0b
srinivas30240 <srinivas.boga@gmail.com>
2023-07-24 11:32:25 +0530

    deleted

--- ABC Technologies/README.md
+++
@@ -1,2 +0,0 @@
-# abctechnologies code
-"# assignment1" 
--- ABC Technologies/src/main/java/com/abc/dataAccessObject/RetailAccessObject.java
+++
@@ -1,9 +0,0 @@
-package com.abc.dataAccessObject;
-
-import com.abc.RetailModule;
-
-public interface RetailAccessObject {
-	void create(RetailModule product);
-	RetailModule read(int product_id);
-
-}
